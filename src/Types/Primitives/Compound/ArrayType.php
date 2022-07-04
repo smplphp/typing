c39c1261f3f411b938d6dd704da11bfe550a7554
Ollie Read <code@ollie.codes>
2022-07-04 13:11:45 +0100

    feat: Added the array compound type
    Refs #3

---
+++ src/Types/Primitives/Compound/ArrayType.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smpl\Typing\Types\Primitives\Compound;
+
+use Smpl\Typing\Concerns\IsChildType;
+use Smpl\Typing\Concerns\IsPrimitiveType;
+use Smpl\Typing\Contracts\ChildType;
+use Smpl\Typing\Contracts\Type;
+use function Smpl\Typing\type_of;
+use function Smpl\Typing\type_of_or_return;
+
+final class ArrayType implements ChildType
+{
+    use IsPrimitiveType, IsChildType {
+        IsPrimitiveType::isAssignableTo as isPrimitiveAssignableTo;
+        IsChildType::isAssignableTo as isParentAssignableTo;
+        IsPrimitiveType::isAssignableFrom insteadof IsChildType;
+    }
+
+    public function __construct()
+    {
+        $this->setParentType(type_of('iterable'));
+    }
+
+    public function getName(): string
+    {
+        return 'array';
+    }
+
+    public function isAssignableTo(string|Type $type): bool
+    {
+        $type = type_of_or_return($type);
+
+        return $this->isPrimitiveAssignableTo($type)
+            || $this->isParentAssignableTo($type);
+    }
+
+    public function isOfType(mixed $value): bool
+    {
+        return is_array($value);
+    }
+
+    public function isCompound(): bool
+    {
+        return true;
+    }
+
+    public function isAlias(): bool
+    {
+        return false;
+    }
+}
