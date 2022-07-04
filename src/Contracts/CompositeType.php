502840609743eb96f1bf83cf42863e12e10d39e4
Ollie Read <code@ollie.codes>
2022-07-04 12:56:15 +0100

    chore: Added base contract for composite types

---
+++ src/Contracts/CompositeType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Smpl\Typing\Contracts;
+
+/**
+ * Multi-type Contract
+ *
+ * This contract represents a type that is either an alias for, or contains
+ * multiple other types.
+ */
+interface CompositeType extends Type
+{
+    /**
+     * Type comparison should use the OR operator.
+     */
+    public const OR  = 0;
+
+    /**
+     * Type comparison should use the AND operator.
+     */
+    public const AND = 1;
+
+    /**
+     * Get the child types for the current type.
+     *
+     * @return \Smpl\Typing\Contracts\Type[]
+     */
+    public function getChildTypes(): array;
+
+    /**
+     * Get the mode to use when comparing types.
+     *
+     * @return int
+     */
+    public function getComparisonMode(): int;
+}
